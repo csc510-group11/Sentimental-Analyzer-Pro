fb6058348fda411bb008cd8522c151db473f187d
Kurt Wilson <git@kurtw.dev>
2025-02-28 19:13:54 -0500

    fix windows-style backslash

--- sentimental_analysis/sentimental_analysis/settings.py
+++ sentimental_analysis/sentimental_analysis/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "realworld\static"),
+    os.path.join(BASE_DIR, "realworld/static"),
 )
 STATIC_URL = '/static/'
 
