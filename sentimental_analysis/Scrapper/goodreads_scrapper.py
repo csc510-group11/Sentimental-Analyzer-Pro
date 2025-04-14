f06a7b8799ecb8e1b596bd798978eacc2b3b0191
NaziaAfreen015 <nazia.afreen015@gmail.com>
2025-04-14 14:54:14 -0400

    scrapper module

---
+++ sentimental_analysis/Scrapper/goodreads_scrapper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_goodreads_reviews(book_url):
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    res = requests.get(book_url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # print(soup)
+
+    book_title = soup.select_one('h1[data-testid="bookTitle"]').get_text(strip=True)
+    description = soup.select_one('div[data-testid="description"]').get_text(strip=True)
+    shelfstatus = soup.select('div[class="ShelfStatus"]')
+    all_reviews = soup.select('section[class="ReviewText__content"]')
+    rating_histogram = soup.select('div[class="RatingsHistogram RatingsHistogram__interactive"]')
+    reactions = {}
+    bars = rating_histogram[0].find_all("div", class_="RatingsHistogram__bar")
+    for bar in bars:
+        star = bar.get("aria-label", "").split()[0]
+        count = bar.select_one(".RatingsHistogram__labelTotal")
+        if star and count:
+            reactions[str(star)+' stars'] = count.get_text(strip=True)
+
+    reviews = []
+    i = 0
+    for review_div in all_reviews:
+        consider = shelfstatus[i].find('span', class_='RatingStars RatingStars__small') if shelfstatus[i] else None
+        if consider == None:
+            i += 1
+            continue
+
+        review_text = review_div.get_text(strip=True)
+        if review_text:
+            rating_text = consider.get("aria-label", None)
+            reviews.append({"review": review_text, "rating": rating_text})
+            i += 1
+
+    return book_title,description,reactions,reviews
+
+# if __name__ == "__main__":
+#     book_url = "https://www.goodreads.com/book/show/2767052-the-hunger-games"
+#     book_title,description,reactions,reviews = scrape_goodreads_reviews(book_url)
+
+ 
+    # for review in reviews:
+    #     print(review, '\n\n\n\n')
+
---
+++ sentimental_analysis/Scrapper/tripadvisor_scrapper.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_tripadvisor(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # print(response.status_code)
+
+    # Get title/heading
+    title = soup.find("h1").get_text(strip=True)
+    description = soup.select_one('div[class="biGQs _P pZUbB avBIb KxBGd"]').get_text(strip=True)
+    rating_histogram = soup.select('div[class="AugPH w u"]')
+    bars = rating_histogram[0].find_all("div", class_="jxnKb")
+    reactions = {}
+    for bar in bars:
+        label = bar.find("div", class_="Ygqck o W q").get_text(strip=True)
+        count = bar.find("div", class_="biGQs _P fiohW biKBZ osNWb").get_text(strip=True)
+        reactions[label] = count
+
+    all_reviews = soup.select('div[class="zwgAY"]')
+    review_cards = all_reviews[0].find_all("div", class_="_c")
+    reviews = []
+    # print(review_cards[0])
+    for card in review_cards:
+        review_title = card.find("div", class_="biGQs _P fiohW qWPrE ncFvv fOtGX").get_text(strip=True)
+        review_text = card.find("div", class_="biGQs _P pZUbB KxBGd").get_text(strip=True) 
+        bubble = card.find("title", string=lambda text: "bubbles" in text).get_text(strip=True)
+        whole_review = review_title+'\n'+review_text
+        reviews.append({"review": whole_review, "rating": bubble})
+
+    return title, description, reactions, reviews
+
+# Example usage
+title, description, reactions, reviews = scrape_tripadvisor("https://www.tripadvisor.com/Restaurant_Review-g34227-d3192530-Reviews-15th_Street_Fisheries-Fort_Lauderdale_Broward_County_Florida.html")
+print(f"Title: {title}")
+print(f"Description: {description}")
+print(f"Reactions: {reactions}")
+print(f"Reviews: {reviews}")
+
