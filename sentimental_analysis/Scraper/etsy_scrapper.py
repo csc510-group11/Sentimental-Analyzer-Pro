bdc4350f8acd180d5e35047c1f143a34c5a9858e
NaziaAfreen015 <nazia.afreen015@gmail.com>
2025-04-15 02:15:38 -0400

    etsy scrapper done

--- sentimental_analysis/Scraper/etsy_scrapper.py
+++ sentimental_analysis/Scraper/etsy_scrapper.py
@@ -13,35 +13,49 @@ def scrape_etsy(url):
     service = Service()
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
-    # options.add_argument('--disable-blink-features=AutomationControlled')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-gpu')
-    options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
-    driver = webdriver.Chrome(service=service,options=options)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    # options.add_argument('--window-size=1920,1080')
+    options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
+    reviews = []
 
-    # input("Solve CAPTCHA manually in the browser, then press Enter here to continue...")
-    time.sleep(1)  # Wait for a few seconds to ensure the page loads completely
     try:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="sc-376e51f1-9 eDjlwl"]'))
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'h1[data-buy-box-listing-title="true"]'))
         )
-        i = 10
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        reactions = {}
-        # r = soup.select('text[id="chart-bar-1-labels-0"]')\
+        title = soup.select_one('h1[data-buy-box-listing-title="true"]').get_text(strip=True)
+        WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div[id="same-listing-reviews-panel"]'))
+        )
+        soup = BeautifulSoup(driver.page_source, "html.parser")
         
-        tspan = soup.select('text[id^="chart-bar-1-labels-"] tspan')
-        for p in tspan:
-            reactions[i] = p.text.split()[0]
-            i -= 1
-        return reactions
+        all_reviews = soup.select('div[id="same-listing-reviews-panel"]')
+        review_cards = all_reviews[0].find_all("div", class_="wt-grid__item-xs-12 review-card")
+        for review in review_cards:
+            stars = review.select('span[class="wt-screen-reader-only"]')[0].get_text(strip=True)
+            review_title = review.select('p[id^="review-preview-toggle-"]')[0].get_text(strip=True)
+        # review_title = reviews_and_stars[0].select('p[id="review-preview-toggle-01744694165"]')
+        # stars = reviews_and_stars[0].select('span[class="wt-screen-reader-only"]')
+            print(stars, review_title)
+            reviews.append({"review": review_title, "rating": stars})
+
+        response_dict = {
+            "title": title,
+            "reviews": reviews
+        }
+        return json.dumps(response_dict, indent=4)
     except Exception as e:
-        print("Histogram not found:")
+        print("Title not found:")
         return None
     finally:
         driver.quit()
 
 
 
-etsy_url = "https://www.etsy.com/listing/1807768177/set-of-3-black-kaws-in-shower-bathroom"
+
+etsy_url = "https://www.etsy.com/listing/1808685200/100-random-programmer-stickers-coding"
+print(scrape_etsy(etsy_url))
